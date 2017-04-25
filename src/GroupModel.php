48e0cfbe719a2fe950ceea5f7e442e43fa6c9c57
Romans Malinovskis <romans@agiletoolkit.org>
2017-04-25 09:41:38 +0100

    Implemented group conditions properly, resolve #13

--- src/GroupModel.php
+++ src/GroupModel.php
@@ -65,7 +65,7 @@ class GroupModel extends \atk4\data\Model
             $field_object = $this->master_model->hasElement($field);
 
             // can be used as part of expression
-            $e = $this->expr($expr, [$field_object]);
+            $e = $this->master_model->expr($expr, [$field_object]);
 
             // now add the expressions here
             $field_object = $this->addExpression($field, $e);
@@ -117,13 +117,6 @@ class GroupModel extends \atk4\data\Model
     public function setLimit($count, $offset = null)
     {
         $this->master_model->setLimit($count, $offset);
-
-        return $this;
-    }
-
-    public function addCondition($field, $operator = NULL, $value = NULL)
-    {
-        $this->master_model->addCondition($field, $operator, $value);
 
         return $this;
     }
@@ -180,6 +173,9 @@ class GroupModel extends \atk4\data\Model
 
                 $this->hook('afterGroupSelect', [$query]);
 
+                $this->initQueryConditions($query);
+                //$this->persistence->setLimitOrder($this, $query);
+
                 return $query;
 
             case 'count':
@@ -233,4 +229,57 @@ class GroupModel extends \atk4\data\Model
         $query->reset('table')->table($subquery);
         return $query;
     }
+    function initQueryConditions($q)
+    {
+        $m = $this;
+        if (!isset($m->conditions)) {
+            // no conditions are set in the model
+            return $q;
+        }
+
+        foreach ($m->conditions as $cond) {
+
+            // Options here are:
+            // count($cond) == 1, we will pass the only
+            // parameter inside where()
+
+            if (count($cond) == 1) {
+
+                // OR conditions
+                if (is_array($cond[0])) {
+                    foreach ($cond[0] as &$row) {
+                        if (is_string($row[0])) {
+                            $row[0] = $m->getElement($row[0]);
+                        }
+                    }
+                }
+
+                $q->where($cond[0]);
+                continue;
+            }
+
+            if (is_string($cond[0])) {
+                $cond[0] = $m->getElement($cond[0]);
+            }
+
+            if (count($cond) == 2) {
+                if ($cond[0] instanceof \atk4\data\Field) {
+                    $cond[1] = $this->persistence->typecastSaveField($cond[0], $cond[1]);
+                    $q->having($cond[0]->actual ?: $cond[0]->short_name, $cond[1]);
+                } else {
+                    $q->where($cond[0], $cond[1]);
+                }
+            } else {
+                if ($cond[0] instanceof \atk4\data\Field) {
+                    $cond[2] = $this->persistence->typecastSaveField($cond[0], $cond[2]);
+                    $q->having($cond[0]->actual ?: $cond[0]->short_name, $cond[1], $cond[2]);
+                } else {
+                    $q->where($cond[0], $cond[1], $cond[2]);
+                }
+            }
+        }
+
+        return $q;
+    }
+
 }
---
+++ tests/GroupTest.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace atk4\report\tests;
+
+
+/**
+ * Tests basic create, update and delete operatiotns
+ */
+class GroupTest extends \atk4\schema\PHPUnit_SchemaTestCase
+{
+
+    private $init_db = 
+        [
+            'client' => [
+                ['name' => 'Vinny'],
+                ['name' => 'Zoe'],
+            ],
+            'invoice' => [
+                ['client_id'=>1, 'name'=>'chair purchase', 'amount'=>4],
+                ['client_id'=>1, 'name'=>'table purchase', 'amount'=>15],
+                ['client_id'=>2, 'name'=>'chair purchase', 'amount'=>4],
+            ],
+            'payment' => [
+                ['client_id'=>1, 'name'=>'prepay', 'amount'=>10],
+                ['client_id'=>2, 'name'=>'full pay', 'amount'=>4],
+            ],
+        ];
+
+    protected $g;
+
+    function setUp() {
+        parent::setUp();
+        $m1 = new Invoice($this->db);
+        $m1->getRef('client_id')->addTitle();
+        $this->g = new \atk4\report\GroupModel($m1);
+        $this->g->addField('client');
+    }
+
+    public function testGroupSelect()
+    {
+        $g = $this->g;
+
+        $g->groupBy(['client_id'], ['c'=>'count(*)']);
+
+        $this->assertEquals(
+            [
+                ['client'=>'Vinny','client_id'=>1, 'c'=>2],
+                ['client'=>'Zoe','client_id'=>2, 'c'=>1],
+            ],
+            $g->export()
+        );
+    }
+
+    public function testGroupSelect2()
+    {
+        $g = $this->g;
+
+        $g->groupBy(['client_id'], [
+            //'s'=>'sum([amount])',
+            'amount'=>'sum([])',
+        ]);
+
+        $this->assertEquals(
+            [
+                ['client'=>'Vinny','client_id'=>1, 'amount'=>19],
+                ['client'=>'Zoe','client_id'=>2, 'amount'=>4],
+            ],
+            $g->export()
+        );
+    }
+
+    public function testGroupSelect3()
+    {
+        $g = $this->g;
+
+        $g->groupBy(['client_id'], [
+            's'=>'sum([amount])',
+            'amount'=>'sum([])',
+        ]);
+
+        $this->assertEquals(
+            [
+                ['client'=>'Vinny','client_id'=>1, 'amount'=>19, 's'=>19],
+                ['client'=>'Zoe','client_id'=>2, 'amount'=>4, 's'=>4],
+            ],
+            $g->export()
+        );
+    }
+
+    public function testGroupSelectExpr()
+    {
+        $g = $this->g;
+
+        $g->groupBy(['client_id'], [
+            's'=>'sum([amount])',
+            'amount'=>'sum([])',
+        ]);
+
+        $g->addExpression('double', '[s]+[amount]');
+
+        $this->assertEquals(
+            [
+                ['client'=>'Vinny','client_id'=>1, 'amount'=>19, 's'=>19, 'double'=>38],
+                ['client'=>'Zoe','client_id'=>2, 'amount'=>4, 's'=>4, 'double'=>8],
+            ],
+            $g->export()
+        );
+    }
+
+    public function testGroupSelectCondition()
+    {
+        $g = $this->g;
+        $g->master_model->addCondition('name', 'chair purchase');
+
+        $g->groupBy(['client_id'], [
+            's'=>'sum([amount])',
+            'amount'=>'sum([])',
+        ]);
+
+        $g->addExpression('double', '[s]+[amount]');
+
+        $this->assertEquals(
+            [
+                ['client'=>'Vinny','client_id'=>1, 'amount'=>4, 's'=>4, 'double'=>8],
+                ['client'=>'Zoe','client_id'=>2, 'amount'=>4, 's'=>4, 'double'=>8],
+            ],
+            $g->export()
+        );
+    }
+
+    public function testGroupSelectCondition2()
+    {
+        $g = $this->g;
+
+        $g->groupBy(['client_id'], [
+            's'=>'sum([amount])',
+            'amount'=>'sum([])',
+        ]);
+
+        $g->addExpression('double', '[s]+[amount]');
+        $g->addCondition('double', '>', 10);
+
+        $this->assertEquals(
+            [
+                ['client'=>'Vinny','client_id'=>1, 'amount'=>19, 's'=>19, 'double'=>38],
+            ],
+            $g->export()
+        );
+    }
+
+    public function testGroupSelectCondition3()
+    {
+        $g = $this->g;
+
+        $g->groupBy(['client_id'], [
+            's'=>'sum([amount])',
+            'amount'=>'sum([])',
+        ]);
+
+        $g->addExpression('double', '[s]+[amount]');
+        $g->addCondition('double', 38);
+
+        $this->assertEquals(
+            [
+                ['client'=>'Vinny','client_id'=>1, 'amount'=>19, 's'=>19, 'double'=>38],
+            ],
+            $g->export()
+        );
+    }
+
+    public function testGroupSelectCondition4()
+    {
+        $g = $this->g;
+
+        $g->groupBy(['client_id'], [
+            's'=>'sum([amount])',
+            'amount'=>'sum([])',
+        ]);
+
+        $g->addExpression('double', '[s]+[amount]');
+        $g->addCondition('client_id', 2);
+
+        $this->assertEquals(
+            [
+                ['client'=>'Zoe','client_id'=>2, 'amount'=>4, 's'=>4, 'double'=>8],
+            ],
+            $g->export()
+        );
+    }
+}
